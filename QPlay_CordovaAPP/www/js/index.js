84df6bcbf1921023e02d7a660a0a71f6239a6a6a
faulfish <faulfish@gmail.com>
2016-05-31 16:36:43 +0800

    Add QPlay & YellowPage

---
+++ QPlay_CordovaAPP/www/js/index.js
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+ $(document).ready(function () {
+    var owl = $('#applist1');
+
+    $('#applist1').owlCarousel({
+        items:4,
+        loop:false,
+        margin:10
+    });
+
+    $('#applist2').owlCarousel({
+        stagePadding: 50,
+        loop:false,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    $( "#dialog" ).dialog({ autoOpen: false });
+
+    function callback(event) {
+
+        $( "#dialog" ).dialog( "open" );
+
+        // Provided by the core
+        var element   = event.target;         // DOM element, in this example .owl-carousel
+        var name      = event.type;           // Name of the event, in this example dragged
+        var namespace = event.namespace;      // Namespace of the event, in this example owl.carousel
+        var items     = event.item.count;     // Number of items
+        var item      = event.item.index;     // Position of the current item
+        // Provided by the navigation plugin
+        var pages     = event.page.count;     // Number of pages
+        var page      = event.page.index;     // Position of the current page
+        var size      = event.page.size;      // Number of items per page
+
+        jQuery("#TicketTC")
+          .on("dialogopen", function (event, ui) {
+            setupCheckBox('chk-img');
+          })
+          .html('<div class="support-msg"></div>')
+          .dialog({
+            modal: true,
+            width: 200,
+            height: 'auto',
+            autoOpen: true,
+            buttons: {
+              CONTINUE: function () {
+                jQuery(this).dialog("close");
+                return true;
+              },
+              CANCEL: function () {
+                jQuery(this).dialog("close");
+                return false;
+              }
+            }
+          });
+    }
+
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+
+ });
+
+ $( document ).on( "click", ".show-page-loading-msg", function() {
+     var $this = $( this ),
+         theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
+         msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
+         textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
+         textonly = !!$this.jqmData( "textonly" );
+         html = $this.jqmData( "html" ) || "";
+     $.mobile.loading( "show", {
+             text: msgText,
+             textVisible: textVisible,
+             theme: theme,
+             textonly: textonly,
+             html: html
+     });
+ })
+ .on( "click", ".hide-page-loading-msg", function() {
+     $.mobile.loading( "hide" );
+ });
+ 
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
