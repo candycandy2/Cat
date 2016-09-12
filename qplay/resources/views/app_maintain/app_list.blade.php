ed4959108d3a69a6018eb37e0e845671111778e6
CleoWChan <Cleo.W.Chan@BenQ.com>
2016-09-12 16:10:13 +0800

    bug fixed
    1.fixed getAppList status bug
    2.changed the style display of dialog text box

--- qplay/resources/views/app_maintain/app_list.blade.php
+++ qplay/resources/views/app_maintain/app_list.blade.php
@@ -10,7 +10,7 @@ $menu_name = "APP_MAINTAIN";
         </button>
     </div>
      <table id="gridAppList" class="bootstrapTable" data-toggle="table" data-sort-name="row_id" data-toolbar="#toolbar"
-           data-url="" data-height="398" data-pagination="true"
+           data-url="" data-height="600" data-pagination="true"
            data-show-refresh="true" data-row-style="rowStyle" data-search="true"
            data-show-toggle="true"  data-sortable="true"
            data-striped="true" data-page-size="10" data-page-list="[5,10,20]"
@@ -65,7 +65,7 @@ $menu_name = "APP_MAINTAIN";
 
 
 @section('dialog_content')
-    <div id="newAppDialog" class="modal fade">
+<div id="newAppDialog" class="modal fade">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -73,35 +73,41 @@ $menu_name = "APP_MAINTAIN";
                     <h1 class="modal-title" id="newAppDialogTitle"></h1>
                 </div>
                 <div class="modal-body">
-                    <table>
-                    	<tr>
-                            <td>{{trans("messages.APP_KEY")}}:</td>
-                            <td style="padding: 10px;">
-	                            <select name="ddlAppKey" id="ddlAppKey">
-					                @foreach($data['projectInfo'] as $pInfo)
-					                    <option value="{{$pInfo->row_id}}">{{$pInfo->app_key}}</option>
-					                @endforeach
-					            </select>
-				            </td>
-                        </tr>
-                        <tr>
-                            <td>{{trans("messages.APP_NAME")}}:</td>
-                            <td style="padding: 10px;">
-                                <input type="text" data-clear-btn="true" name="tbxAppName"
-                                       id="tbxAppName" value=""/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{{trans("messages.DEFAULT_LANG")}}:</td>
-                            <td style="padding: 10px;">
-	                            <select name="ddlLang" id="ddlLang" onchange="">
-					                @foreach($data['langList'] as $lList)
-					                    <option value="{{$lList->row_id}}">{{$lList->lang_desc}}</option>
-					                @endforeach
-					            </select>
-				            </td>
-                        </tr>
-                    </table>
+                    <form id="newAppForm">
+                        <table style="width:100%">
+                            <tr>
+                                <td>{{trans("messages.APP_KEY")}}:</td>
+                                <td style="padding: 10px;">
+                                    <select name="ddlAppKey" id="ddlAppKey" class="form-control">
+                                        <option value="" disabled selected>{{trans("messages.SELECT_APP_KEY")}}</option>
+                                        @foreach($data['projectInfo'] as $pInfo)
+                                            <option value="{{$pInfo->row_id}}">{{$pInfo->app_key}}</option>
+                                        @endforeach
+                                    </select>
+                                </td>
+                                <td><span style="color: red;">*</span></td>
+                            </tr>
+                            <tr>
+                                <td>{{trans("messages.APP_NAME")}}:</td>
+                                <td style="padding: 10px;">
+                                    <input type="text" data-clear-btn="true" class="form-control" name="tbxAppName"
+                                           id="tbxAppName" value="" required="required"/>
+                                </td>
+                                <td><span style="color: red;">*</span></td>
+                            </tr>
+                            <tr>
+                                <td>{{trans("messages.DEFAULT_LANG")}}:</td>
+                                <td style="padding: 10px;">
+                                    <select name="ddlLang" id="ddlLang" onchange="" class="form-control" required="required">
+                                        @foreach($data['langList'] as $lList)
+                                            <option value="{{$lList->row_id}}">{{$lList->lang_desc}}</option>
+                                        @endforeach
+                                    </select>
+                                </td>
+                                <td><span style="color: red;">*</span></td>
+                            </tr>
+                        </table>
+                    </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button"  class="btn btn-danger" onclick="sendMainInfo()">{{trans("messages.NEW")}}</button>
--- qplay/resources/views/app_maintain/category_apps_maintain.blade.php
+++ qplay/resources/views/app_maintain/category_apps_maintain.blade.php
@@ -43,7 +43,7 @@ $categoryInfo = \App\lib\CommonUtil::getCategoryInfoByRowId($categoryId);
             </div>
 
             <table id="gridAppList" class="bootstrapTable" data-toggle="table" data-sort-name="row_id" data-toolbar="#toolbar"
-                   data-url="AppMaintain/getCategoryAppsList?category_id={{$categoryId}}" data-height="398" data-pagination="true"
+                   data-url="AppMaintain/getCategoryAppsList?category_id={{$categoryId}}" data-height="600" data-pagination="true"
                    data-show-refresh="true" data-row-style="rowStyle" data-search="true"
                    data-show-toggle="true"  data-sortable="true"
                    data-striped="true" data-page-size="10" data-page-list="[5,10,20]"
