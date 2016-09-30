94093cac71e8c1a9fc6d11720fefdbe59f963bfd
alan tu <alan.tu@benq.com>
2016-09-30 17:53:57 +0800

    apply ID by gulp

---
+++ APP/NewQPlay/gulpfile.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////
+//Install
+//windows =>npm install -g gulp
+//mac =>sudo npm install -g gulp
+//=>npm install --save-dev gulp
+//=>npm install gulp-concat gulp-less gulp-uglify gulp-copy
+//UglifyJS – a JavaScript parser/compressor/beautifier
+////////////////////////////////////////
+//Run
+//By default gulpfile.js default
+//=>gulp
+////////////////////////////////////////
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var copy = require('gulp-copy');
+
+
+gulp.task('copy',function(){
+    return gulp.src('Images/iOS/**/*')
+        .pipe(gulp.dest('platforms/ios/QPlay/Images.xcassets'));
+});
+
+gulp.task('less',function(){
+    return gulp.src('www/src/css/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('www/src/css/'));
+});
+
+gulp.task('concat:css', ['less'], function(){
+    return gulp.src('www/src/css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('www/dist/css'));
+});
+
+gulp.task('concat:js', function(){
+    return gulp.src(['www/src/js/config.js','src/js/hello.js','src/js/main.js'])
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('www/dist/js'));
+});
+
+gulp.task('default', ['concat:js', 'concat:css'], function(){
+    return gulp.src('www/src/index.html')
+        .pipe(gulp.dest('www/dist'));
+});
