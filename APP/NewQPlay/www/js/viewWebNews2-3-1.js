5aaaaafc499736cc9101550d94873b30b85c349c
Darren Ti <Darren.K.Ti@BenQ.com>
2016-10-20 16:50:43 +0800

    New QPlay
    1. update for Event/News detail page.

--- APP/NewQPlay/www/js/viewWebNews2-3-1.js
+++ APP/NewQPlay/www/js/viewWebNews2-3-1.js
@@ -55,6 +55,73 @@ $(document).one("pagecreate", "#viewWebNews2-3-1", function(){
 
             $("#viewWebNews2-3-1").one("pageshow", function(event, ui) {
 
+                //RWD header & footer
+                var header = {
+                    width: 600,
+                    height: 172
+                };
+
+                var footerImage = {
+                    width: 98,
+                    height: 65
+                };
+
+                var tempWidth;
+                var tempHeight;
+                var tempTop;
+                var footerWidth;
+                var footerHeight;
+
+                tempWidth = $("#viewWebNews2-3-1 .news-header").width();
+                tempHeight = header['height'] * tempWidth / header['width'];
+                $("#viewWebNews2-3-1 .news-header").css('height', tempHeight + 'px');
+
+                tempTop = tempHeight * 0.2;
+                $("#viewWebNews2-3-1 .priority-title").css('top', tempTop + 'px');
+
+                tempTop = tempHeight * 0.35;
+                $("#viewWebNews2-3-1 .header-date").css('top', tempTop + 'px');
+
+                footerWidth = $("#viewWebNews2-3-1 .footer-top").width();
+
+                if (footerWidth * 0.2 < footerImage['width']) {
+                    tempWidth = footerWidth * 0.2;
+                    tempHeight = footerImage['height'] * tempWidth / footerImage['width'];
+
+                    $("#viewWebNews2-3-1 .footer-top").css('height', tempHeight + 'px');
+
+                    $("#viewWebNews2-3-1 .footer-benq").css({
+                        'width': tempWidth + 'px',
+                        'height': tempHeight + 'px'
+                    });
+                    $("#viewWebNews2-3-1 .footer-qisda").css({
+                        'width': tempWidth + 'px',
+                        'height': tempHeight + 'px'
+                    });
+
+                    $("#viewWebNews2-3-1 .footer-text").css({
+                        'width': (footerWidth - tempWidth * 2) - 15 + 'px',
+                        'padding-top': tempHeight * 0.4 + 'px',
+                        'font-size': '10px'
+                    });
+                } else {
+                    $("#viewWebNews2-3-1 .footer-top").css('height', footerImage['height'] + 'px');
+
+                    $("#viewWebNews2-3-1 .footer-benq").css({
+                        'width': footerImage['width'] + 'px',
+                        'height': footerImage['height'] + 'px'
+                    });
+                    $("#viewWebNews2-3-1 .footer-qisda").css({
+                        'width': footerImage['width'] + 'px',
+                        'height': footerImage['height'] + 'px'
+                    });
+
+                    $("#viewWebNews2-3-1 .footer-text").css({
+                        'width': (footerWidth - footerImage['width'] * 2) - 15 + 'px',
+                        'padding-top': footerImage['height'] * 0.4 + 'px',
+                        'font-size': '12px'
+                    });
+                }
             });
 
             /********************************** dom event *************************************/
