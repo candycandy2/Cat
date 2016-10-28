a4df5ff97bec187aad7384ca88f45ea6b183c55d
MosesZhu <Moses.Zhu@qisda.com>
2016-10-28 15:05:18 +0800

    fix bug for yellowpage api

--- qplayApi/qplayApi/resources/views/login.blade.php
+++ qplayApi/qplayApi/resources/views/login.blade.php
@@ -146,6 +146,7 @@
                                 + '"uuid" : "' + d.content.uuid + '", '
                                 + '"redirect-uri" : "' + d.content.redirect_uri + '", '
                                 + '"token" : "' + d.content.token + '", '
+                                + '"login_id" : "' + d.content.login_id + '", '
                                 + '"emp_no" : "' + d.content.emp_no + '",'
                                 + '"domain" : "' + d.content.domain + '",'
                                 + '"checksum" : "' + d.content.checksum + '",'
