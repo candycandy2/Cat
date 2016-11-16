3f806ab9e70ed04d08071f9a83768470bddaeefe
alan tu <alan.tu@benq.com>
2016-11-16 13:35:30 +0800

    add objects sample

--- componentapp/platforms/ios/qhash/qhash.h
+++ componentapp/platforms/ios/qhash/qhash.h
@@ -5,11 +5,13 @@
 //  Created by alan on 15/11/2016.
 //
 //
+//Sample
+//NSLog(@"timeHash %@",[qhash timeHash:@"swexuc453refebraXecujeruBraqAc4e"]);
 
 #import <Foundation/Foundation.h>
 
 @interface qhash : NSObject
 
-+ (NSString*)timeHash:(NSString*)appkey;
++ (NSString*)timeHash:(NSString*)key;
 
 @end
