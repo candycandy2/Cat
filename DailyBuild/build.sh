7da0cef7e890b8c496a28ad4ce52a29047827e70
samuelhsieh <samuel.hsieh@qisda.com>
2016-12-09 12:27:05 +0800

    build android app of release version

--- DailyBuild/build.sh
+++ DailyBuild/build.sh
@@ -2,28 +2,28 @@ pwd
 cd APP/NewQPlay
 pwd
 
-dailyver=32
+dailyver=34
 
 gulp jenkinsinstall --env test
 gulp --env test --vname 1.0.0.$dailyver --vcode $dailyver
-cordova build android
+cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
 
 cd ../NewYellowPage
 pwd
 gulp jenkinsinstall --env test
 gulp --env test --vname 1.0.0.$dailyver --vcode $dailyver
-cordova build android
+cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
 
 cd ../RRS
 pwd
 gulp jenkinsinstall --env test
 gulp --env test --vname 1.0.0.$dailyver --vcode $dailyver
-cordova build android
+cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
 
 binfolder=~/Documents/QPlayDailyBuild/1.0.0.$dailyver
 mkdir $binfolder
 
 appfolder=~/.jenkins/workspace/QPlayDailyBuild/APP
-cp $appfolder/NewQPlay/platforms/android/build/outputs/apk/android-debug.apk $binfolder/appqplay.apk
-cp $appfolder/NewYellowPage/platforms/android/build/outputs/apk/android-debug.apk $binfolder/appyellowpage.apk
-cp $appfolder/RRS/platforms/android/build/outputs/apk/android-debug.apk $binfolder/apprrs.apk
+cp $appfolder/NewQPlay/platforms/android/build/outputs/apk/android-release.apk $binfolder/appqplay.apk
+cp $appfolder/NewYellowPage/platforms/android/build/outputs/apk/android-release.apk $binfolder/appyellowpage.apk
+cp $appfolder/RRS/platforms/android/build/outputs/apk/android-release.apk $binfolder/apprrs.apk
