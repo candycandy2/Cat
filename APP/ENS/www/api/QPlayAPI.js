a5f327df58414d0bd5ff1771d91c5000a9d60f4e
Candy.XC.Lee <Candy.XC.Lee@qisda.com>
2017-01-06 09:06:21 +0800

    ENS APP create
    ENS APP create

---
+++ APP/ENS/www/api/QPlayAPI.js
@@ -0,0 +1,38 @@
+//qplayAPI - YellowPage 
+//TODO: need to chang for ENS
+function QPlayAPI(requestType, requestAction, successCallback, failCallback, queryData) {
+
+    failCallback = failCallback || null;
+    queryData = queryData || null;
+
+    function requestSuccess(data) {
+        checkTokenValid(data['ResultCode'], data['token_valid'], successCallback, data);
+    }
+
+    function requestError(data) {
+        checkNetwork();
+    }
+
+    var signatureTime = getSignature("getTime");
+    var signatureInBase64 = getSignature("getInBase64", signatureTime);
+
+    $.ajax({
+        type: requestType,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'App-Key': appKey,
+            'Signature-Time': signatureTime,
+            'Signature': signatureInBase64,
+            'token': loginData.token
+        },
+  	    url: serverURL + "/" + appApiPath + "/public/v101/custom/yellowpage/" + requestAction + "?lang=en-us&uuid=" + loginData.uuid,
+        //url: serverURL + "/" + appApiPath + "/public/index.php/v101/yellowpage/" + requestAction + "?lang=en-us&uuid=" + loginData.uuid,
+        dataType: "json",
+        data: queryData,
+        cache: false,
+        timeout: 3000,
+        success: requestSuccess,
+        error: requestError
+    });
+    
+}
