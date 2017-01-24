9f22ef0d88e4078a336b7788facb5dd49656cc0f
VinnyTang <Vinny.YC.Tang@BenQ.com>
2017-01-24 19:15:35 +0800

    [EIS] redesign the style of panel in viewHitRate page

--- APP/EIS/www/js/index.js
+++ APP/EIS/www/js/index.js
@@ -8,18 +8,18 @@ var appSecretKey = "af8973de05c940f98a2c5e20b2ba649b";
 
 var htmlContent = "";
 var panel = htmlContent
-        +'<div data-role="panel" id="mypanel" data-display="push" data-theme="a" style="background-color:#cecece">'
+        +'<div data-role="panel" id="mypanel" data-display="overlay" style="background-color:#cecece; box-shadow:0 0 0;">'
         +   '<div id="panel-header">'
-        +       '<p class="panel-text" style="line-height:7VH;">Sales Analysis</p>'
+        +       '<span class="panel-text" style="line-height:7.5VH;">Sales Analysis</span>'
         +   '</div>'
-        +   '<div>'   
-        +       '<p class="panel-text" style="font-size: 3.5VW;">Hit Rate</p>'
+        +   '<div class="panel-content">'   
+        +       '<span class="panel-text" style="line-height:7.5VH;">Hit Rate</span>'
         +   '</div>'
-        +   '<div>'
-        +        '<p class="panel-text" style="font-size: 3.5VW;">Monthly Hit Rate Trend</p>'
+        +   '<div id="panel-sub-header">'
+        +        '<span class="panel-text" style="line-height:7.5VH;">Monthly Hit Rate Trend</span>'
         +   '</div>'
-        +   '<div>'
-        +        '<p class="panel-text" style="font-size: 3.5VW;">YTD Hit Rate Trend</p>'
+        +   '<div class="panel-content" id="underscore">'
+        +        '<span class="panel-text" style="line-height:7.5VH;">YTD Hit Rate Trend</span>'
         +   '</div>'
         +'</div>';
 
