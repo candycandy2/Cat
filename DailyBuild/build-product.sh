98f72c7c3dc29d32ecdeca2347d2f57338f7e0c8
samuelhsieh <samuel.hsieh@qisda.com>
2017-02-08 16:37:36 +0800

    modify provisioning profile

--- DailyBuild/build-product.sh
+++ DailyBuild/build-product.sh
@@ -26,18 +26,18 @@ cd ../NewYellowPage
 pwd
 gulp config --env test --vname 1.0.0.$dailyver --vcode $dailyver
 gulp jenkinsinstall --env test
-gulp default
+gulp jenkinsdefault
 cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
-cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="5e4f62e9-a983-4971-bb73-f83be5235c43" --packageType="enterprise"
+cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="2953da8d-cd58-4d83-b2f9-90b93602b312" --packageType="enterprise"
 
 # ------ build RRS Staging ------
 cd ../RRS
 pwd
 gulp config --env test --vname 1.0.0.$dailyver --vcode $dailyver
 gulp jenkinsinstall --env test
-gulp default
+gulp jenkinsdefault
 cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
-cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="148bd575-9e03-42cf-b5f3-ca50132226bd" --packageType="enterprise"
+cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="afbbaf29-754b-44ac-9ddf-09c516c6d3d0" --packageType="enterprise"
 
 # make directory of Staging for apk and ipa
 binfolder=~/Documents/QPlayStaging/1.0.0.$dailyver
@@ -56,7 +56,6 @@ rm -Rf ~/Documents/QPlayStaging/QPlayStaging/
 cp -R ~/.jenkins/workspace/QPlayProduct ~/Documents/QPlayStaging/QPlayStaging/
 
 
-
 # ------ build QPlay Production ------
 cd ../../Production/NewQPlay
 gulp config --vname 1.0.0.$dailyver --vcode $dailyver
@@ -69,16 +68,16 @@ cd ../NewYellowPage
 pwd
 gulp config --vname 1.0.0.$dailyver --vcode $dailyver
 gulp jenkinsinstall
-gulp default
+gulp jenkinsdefault
 cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
-cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="03d61647-a929-4b9f-8f10-325df7ae88f4" --packageType="enterprise"
+cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="a2ff443f-a7d0-49cd-947f-f621af2723ca" --packageType="enterprise"
 
 # ------ build RRS Production ------
 cd ../RRS
 pwd
 gulp config --vname 1.0.0.$dailyver --vcode $dailyver
 gulp jenkinsinstall
-gulp default
+gulp jenkinsdefault
 cordova build android --release -- --keystore=~/keystores/android.jks --storePassword=BenQ1234 --alias=QPlayAndroidKey --password=BenQ1234
 cordova build ios --device --codeSignIdentity="iPhone Distribution" --provisioningProfile="cf3b7828-ee64-4338-8e84-837e5f54367c" --packageType="enterprise"
 
@@ -106,6 +105,7 @@ cp -R ~/.jenkins/workspace/QPlayProduct ~/Documents/QPlayProduct/QPlayProduct/
 # ------ commit modified files ------
 #cd ..
 #pwd
+#git pull
 #git add NewQPlay/config.xml
 #git add NewYellowPage/config.xml
 #git add RRS/config.xml
