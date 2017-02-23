da1dd246ac86bfd799dd49cccc38fcd66c78594e
Candy.XC.Lee <Candy.XC.Lee@qisda.com>
2017-02-23 13:10:07 +0800

    Add New APP for qplay20170223
    Add New APP for qplay

--- APP/NewQPlay/gulpfile.js
+++ APP/NewQPlay/gulpfile.js
@@ -58,6 +58,9 @@ var schemeSetting =   "<string>appqplay"    + appNameDecorate + "</string>"
                     + "<string>appens"      + appNameDecorate + "</string>"
                     + "<string>appeis"      + appNameDecorate + "</string>"
                     + "<string>appleave"    + appNameDecorate + "</string>"
+					+ "<string>apprelieve"  + appNameDecorate + "</string>"
+					+ "<string>appmas"    + appNameDecorate + "</string>"
+					+ "<string>appim"    + appNameDecorate + "</string>"
                     + "<string>appscheme01" + appNameDecorate + "</string>"
                     + "<string>appscheme02" + appNameDecorate + "</string>"
                     + "<string>appscheme03" + appNameDecorate + "</string>"
@@ -106,6 +109,9 @@ var configContent =   '<?xml version="1.0" encoding="utf-8"?>' +
                         '<allow-intent href="appens' +      appNameDecorate + ':*" />' +
                         '<allow-intent href="appaccountingrate'+appNameDecorate + ':*" />' +
 						'<allow-intent href="appleave'+appNameDecorate + ':*" />' +
+						'<allow-intent href="apprelieve'+appNameDecorate + ':*" />' +
+						'<allow-intent href="appmas'+appNameDecorate + ':*" />' +
+						'<allow-intent href="appim'+appNameDecorate + ':*" />' +
                         '<allow-intent href="appscheme01' + appNameDecorate + ':*" />' +
                         '<allow-intent href="appscheme02' + appNameDecorate + ':*" />' +
                         '<allow-intent href="appscheme03' + appNameDecorate + ':*" />' +
@@ -148,7 +154,7 @@ gulp.task('config', function(){
 
 //ex: gulp install --env test
 gulp.task('install', shell.task([
-    'cordova plugin remove cordova-plugin-qlogin',
+/*     'cordova plugin remove cordova-plugin-qlogin',
     'cordova plugin remove cordova-plugin-qpush',
     'cordova plugin remove cordova-plugin-device',
     'cordova plugin remove cordova-plugin-console',
@@ -160,8 +166,8 @@ gulp.task('install', shell.task([
     'cordova plugin remove cordova-plugin-appavailability',
     //'cordova plugin remove cordova-plugin-file',
     'cordova platform rm ios',
-    'cordova platform rm android',
-    'cordova platform add ios',
+    'cordova platform rm android', */
+/*     'cordova platform add ios', */
     'cordova platform add android',
     'cordova plugin add ../../plugins/cordova-plugin-qlogin --variable LOGIN_URL=' + apiServerURL + 'qplayApi/public/qplayauth_register',
     'cordova plugin add ../../plugins/cordova-plugin-qpush --variable API_KEY=' + QPushAPPKey,
