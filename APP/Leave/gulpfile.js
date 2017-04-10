3fe21413ce27fa39bbcf3f55946acac652d6c064
samuelhsieh <samuel.hsieh@qisda.com>
2017-04-10 15:46:49 +0800

    [DailyBuild] modify gulpfile to avoid multi build conflict

--- APP/Leave/gulpfile.js
+++ APP/Leave/gulpfile.js
@@ -26,7 +26,7 @@ function getArg(key) {
     return (index < 0) ? null : (!next || next[0] === "-") ? true : next;
 }
 
-var env = getArg("--env");  
+var env = getArg("--env");
 var vname = getArg("--vname");
 var vcode = getArg("--vcode");
 
@@ -104,8 +104,8 @@ gulp.task('install', shell.task([
   'cordova plugin remove cordova-plugin-whitelist',
   'cordova plugin remove cordova-plugin-file',
   'cordova platform rm ios',
-  'cordova platform rm android', 
-  'cordova platform add ios', 
+  'cordova platform rm android',
+  'cordova platform add ios',
   'cordova platform add android',
   */
   'cordova plugin add cordova-plugin-device',
@@ -178,17 +178,17 @@ gulp.task('concat:css', ['less'], function(){
 gulp.task('templateHTML', function() {
     return gulp.src('../component/template/*.html')
         .pipe(concat('template.html'))
-        .pipe(gulp.dest('../component/'));
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('appHTML', ['templateHTML'], function(){
-    return gulp.src(['../component/component.html','../component/template.html'])
+    return gulp.src(['../component/component.html','./template.html'])
         .pipe(concat('APP.html'))
         .pipe(gulp.dest('www/View/'));
 });
 
 gulp.task('componentHTML', ['appHTML'], shell.task([
-    'rm ../component/template.html'
+    'rm ./template.html'
 ]));
 
 gulp.task('componentIMG', function() {
@@ -204,11 +204,11 @@ gulp.task('libJS', function() {
 gulp.task('functionJS', function() {
     return gulp.src('../component/function/*.js')
         .pipe(concat('function.js'))
-        .pipe(gulp.dest('../component/'));
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('appJS', ['functionJS'], function(){
-    return gulp.src(['../component/component.js','../component/function.js'])
+    return gulp.src(['../component/component.js','./function.js'])
         //.pipe(uglify())
         //.pipe(concat('app.min.js'))
         .pipe(concat('APP.js'))
@@ -226,7 +226,7 @@ gulp.task('String', ['commonString'], function() {
 });
 
 gulp.task('componentJS', ['libJS', 'appJS', 'String'], shell.task([
-    'rm ../component/function.js'
+    'rm ./function.js'
 ]));
 
 //ex: gulp default
