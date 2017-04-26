27cfc73ddd8d7bcf86165b68c0a8c8abb676a648
CleoWChan <Cleo.W.Chan@BenQ.com>
2017-04-26 17:38:30 +0800

    fix app download url

--- qplay/app/lib/FilePath.php
+++ qplay/app/lib/FilePath.php
@@ -7,15 +7,17 @@ class FilePath
 {
    
     public static function getApkUploadPath($appRowId,$deviceType,$versionCode){
+        $deviceType = strtolower($deviceType);
          return base_path(\Config::get('app.upload_path')). DIRECTORY_SEPARATOR .$appRowId. DIRECTORY_SEPARATOR .'apk'. DIRECTORY_SEPARATOR .$deviceType. DIRECTORY_SEPARATOR . $versionCode . DIRECTORY_SEPARATOR ;
     }
 
     public static function getApkUrl($appRowId,$deviceType,$versionCode,$fileName){
+        $deviceType = strtolower($deviceType);
        return url(\Config::get('app.upload_folder')).'/'.$appRowId.'/'.'apk'.'/'.$deviceType.'/'. $versionCode .'/'.$fileName;
     }
 
     public static function getApkDownloadUrl($appRowId,$deviceType,$versionCode,$fileName){
-
+        $deviceType = strtolower($deviceType);
         $url = "";
         if($deviceType == 'ios'){
              $url = self::getApkUrl($appRowId,$deviceType,$versionCode,'manifest.plist');
@@ -28,10 +30,12 @@ class FilePath
     }
 
     public static function getApkPublishFilePath($appRowId,$deviceType){
+        $deviceType = strtolower($deviceType);
         return base_path(\Config::get('app.upload_path')). DIRECTORY_SEPARATOR .$appRowId. DIRECTORY_SEPARATOR .'apk'. DIRECTORY_SEPARATOR .$deviceType. DIRECTORY_SEPARATOR;
     }
 
     public static function getApkPublishUrl($appRowId,$deviceType,$fileName){
+        $deviceType = strtolower($deviceType);
         return url(\Config::get('app.upload_folder')).'/'.$appRowId.'/'.'apk'.'/'.$deviceType.'/'. $versionCode .'/'.$fileName;
     }
 
@@ -44,18 +48,22 @@ class FilePath
     } 
 
     public static function getBannerUploadPath($appRowId,$langRowId,$deviceType){
+        $deviceType = strtolower($deviceType);
         return base_path(\Config::get('app.upload_path')). DIRECTORY_SEPARATOR .$appRowId. DIRECTORY_SEPARATOR .'banner'. DIRECTORY_SEPARATOR .$langRowId. DIRECTORY_SEPARATOR . $deviceType . DIRECTORY_SEPARATOR ;
     }
 
     public static function getBannerUrl($appRowId,$langRowId,$deviceType,$fileName){
+        $deviceType = strtolower($deviceType);
          return url(\Config::get('app.upload_folder')).'/'.$appRowId.'/'.'banner'.'/'.$langRowId.'/'.$deviceType.'/'.$fileName;
     } 
 
     public static function getScreenShotUploadPath($appRowId,$langRowId,$deviceType){
+        $deviceType = strtolower($deviceType);
         return base_path(\Config::get('app.upload_path')). DIRECTORY_SEPARATOR .$appRowId. DIRECTORY_SEPARATOR .'screenshot'. DIRECTORY_SEPARATOR .$langRowId. DIRECTORY_SEPARATOR . $deviceType . DIRECTORY_SEPARATOR ;
     }
 
     public static function getScreenShotUrl($appRowId,$langRowId,$deviceType,$fileName){
+        $deviceType = strtolower($deviceType);
          return url(\Config::get('app.upload_folder')).'/'.$appRowId.'/'.'screenshot'.'/'.$langRowId.'/'.$deviceType.'/'.$fileName;
     } 
 
