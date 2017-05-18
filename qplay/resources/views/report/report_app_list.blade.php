0baddfa400a7ca59c821644912f1eaea864ffb7d
CleoWChan <Cleo.W.Chan@BenQ.com>
2017-05-18 11:14:38 +0800

    fix report app list search app name
    fix report app list search app name

--- qplay/resources/views/report/report_app_list.blade.php
+++ qplay/resources/views/report/report_app_list.blade.php
@@ -17,7 +17,7 @@ $menu_name = "REPORT";
         <thead>
         <tr>
             <th data-field="project_code" data-sortable="true" data-visible="false" data-searchable="false">Project Code</th>
-            <th data-field="icon_url" data-sortable="false" data-formatter="iconFormatter" data-searchable="false">{{trans("messages.APPLICATION_NAME")}}</th>
+            <th data-field="app_name" data-sortable="false" data-formatter="iconFormatter" data-searchable="true">{{trans("messages.APPLICATION_NAME")}}</th>
             <th data-field="android_release" data-formatter="deviceSupportFormatter" data-search-formatter="true">{{trans("messages.SUPPORT_DEVICE_TYPE")}}</th>
             <th data-field="register_rate" >{{trans("messages.REGISTER_USER_COUNT")}}/{{trans("messages.REGISTER_DEVICE_COUNT")}}</th>
             <th data-field="release_status" data-formatter="releaseFormatter" data-sortable="true" data-search-formatter="true" >{{trans("messages.RELEASED")}}</th>
