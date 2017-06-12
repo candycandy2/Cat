b58130e96b2c901c4f25ad9be06f58fcf12e820f
CleoWChan <Cleo.W.Chan@BenQ.com>
2017-06-12 09:55:51 +0800

    統計報表使用session storage暫存

--- qplay/resources/views/report/api_report/chart/stock_multi_line.blade.php
+++ qplay/resources/views/report/api_report/chart/stock_multi_line.blade.php
@@ -2,86 +2,6 @@
     <div id="container_stock" style="height: 400px; min-width: 310px"></div>
 </div>
 <script>
-
-var createApiLogMultiLine = function(){
-
-    var mydata = {app_key:"{{$data['app_key']}}"},
-    reportEndDate = '{{$data['reportEndDate']}}',
-    mydataStr = $.toJSON(mydata),
-    callUsersData=[],
-    callTimesData=[],
-    seriesOptions = [],
-    result;
-    $.ajax({
-      url:"reportDetail/getCallApiReport",
-      type:"POST",
-      dataType:"json",
-      contentType: "application/json",
-      data:mydataStr,
-      success: function(r){
-            if(!$.isEmptyObject(r)){
-                result = r;
-                var res=[];
-                //arrange call api data
-                $.each(r,function(i,data){
-                    var d = data._id;
-                    if(!res.hasOwnProperty(d.created_at)){
-                        res[d.created_at] = {'times' : 0, 'users' : [], 'actions':[]};
-                    }
-                    res[d.created_at].times =  parseInt(res[d.created_at].times) + parseInt(data.count);
-                  
-                    if($.inArray(d.user_row_id,  res[d.created_at].users) == -1){
-                        res[d.created_at].users.push(d.user_row_id);
-                    }
-
-                    if(!res[d.created_at].actions.hasOwnProperty(d.action)){
-                        res[d.created_at].actions[d.action] = [];
-                    }
-                    var actionArray =  res[d.created_at].actions[d.action];
-                    if(!actionArray.hasOwnProperty(d.company_site)){
-                        actionArray[d.company_site] = [];
-                    }
-                    var companySiteArray =  actionArray[d.company_site];
-                    if(!companySiteArray.hasOwnProperty(d.department)){
-                        companySiteArray[d.department] = {'times' : 0, 'users' : []};
-                    }
-                    var departmentArray =  companySiteArray[d.department];
-                    departmentArray.times =  parseInt(departmentArray.times) + parseInt(data.count);
-                    if($.inArray(d.user_row_id,  departmentArray.users) == -1){
-                        departmentArray.users.push(d.user_row_id);
-                    }
-                });
-                
-                for (var k in res){
-                    if (typeof res[k] !== 'function') {
-                         var tmpTimesArr = [dateToUTC(k),res[k].times];
-                         var tmpUsersArr = [dateToUTC(k),res[k].users.length];
-                         callTimesData.push(tmpTimesArr);
-                         callUsersData.push(tmpUsersArr);
-                         callUsersData = sortObject(callUsersData);
-                         callTimesData = sortObject(callTimesData);
-                    }
-                }
-                seriesOptions[0] = {
-                    name:"呼叫次數",
-                    data:callTimesData
-                }
-
-                seriesOptions[1] = {
-                    name:"呼叫人數",
-                    data:callUsersData
-                }
-                createdDunutChart('container_donut_chart_t');
-                createdDunutChart('container_donut_chart_d');
-                createMultiLineChart('container_stock',seriesOptions, res);
-                createTableChart(res,reportEndDate);
-               
-            }
-       }
-    });
-
-
-}
 /**
  * Create the chart when all data is loaded
  */
@@ -204,11 +124,4 @@ function createMultiLineChart(container, seriesOptions, res) {
 
 }
 
-
-function sortObject(obj){
-    obj = obj.sort(function (a, b) {
-     return a[0] < b[0] ? -1 : 1;
-    });
-    return obj;
-}
 </script>
