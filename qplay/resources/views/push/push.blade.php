6edfbdc8b7a84a2a92151764035e37551a76ff70
CleoWChan <Cleo.W.Chan@BenQ.com>
2017-07-05 15:28:54 +0800

    push list add dpartment,site_code,push status column

--- qplay/resources/views/push/push.blade.php
+++ qplay/resources/views/push/push.blade.php
@@ -23,8 +23,11 @@ $menu_name = "PUSH_SERVER";
             <th data-field="message_type" data-sortable="true" data-formatter="messageTypeFormatter">{{trans("messages.MESSAGE_TYPE")}}</th>
             <th data-field="message_title" data-sortable="true" data-formatter="messageTitleFormatter" data-search-formatter="false" data-width="600px" data-class="grid_long_column">{{trans("messages.MESSAGE_TITLE")}}</th>
             <th data-field="created_at" data-sortable="true" data-formatter="createdDateFormatter" >{{trans("messages.CREATED_DATE")}}</th>
+            <th data-field="site_code" data-sortable="true">{{trans("messages.MESSAGE_SITE_CODE")}}</th>
+            <th data-field="department" data-sortable="true">{{trans("messages.MESSAGE_DEPARTMENT")}}</th>
             <th data-field="created_user" data-sortable="true">{{trans("messages.MESSAGE_CREATED_USER")}}</th>
             <th data-field="visible" data-sortable="true">{{trans("messages.PUBLISH_STATUS")}}</th>
+            <th data-field="need_push" data-sortable="true" data-formatter="needPushFormatter">{{trans("messages.PUSH_STATUS")}}</th>
         </tr>
         </thead>
     </table>
@@ -32,7 +35,8 @@ $menu_name = "PUSH_SERVER";
     <script>
         function createdDateFormatter(value, row){
             return convertUTCToLocalDateTime(value);
-        }
+        };
+
         function messageTitleFormatter(value, row) {
             return '<a href="messagePushHistory?message_id=' + row.row_id + '">' + value + '</a>';
         };
@@ -49,6 +53,13 @@ $menu_name = "PUSH_SERVER";
             return value;
         };
 
+        function needPushFormatter(value, row){
+            if(value > 0){
+                return 'Y';
+            }else{
+                return 'N';
+            }
+        };
     </script>
 @endsection
 
