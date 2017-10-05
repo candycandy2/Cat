53ee0cad1f940a25fc5dc54e6c2591621c881173
JenniferYWang <Jennifer.Y.Wang@BenQ.com>
2017-10-05 18:57:25 +0800

    [Parking] Change APP.min.js + override jQuery Mobile: Input TextBox UI

--- APP/Parking/www/js/viewParkingDetailAdd.js
+++ APP/Parking/www/js/viewParkingDetailAdd.js
@@ -1,6 +1,27 @@
 
 $("#viewParkingDetailAdd").pagecontainer({
     create: function(event, ui) {
+
+    	/********************************** function *************************************/
+    	function setDefaultStatus() {
+            $('#newSettingTitle').val('');
+            var defaultSite = $("#newSettingSite option:first").val();
+            $("#newSettingSite").val(defaultSite).change();
+            $("#newSettingSite option:first").attr("selected", "selected");
+            $('#newSettingPeople input[value=0]').prop("checked", "checked");
+            $('#newSettingPeople input[id^=num-]').checkboxradio("refresh");
+            $('#newSettingTime input[id=setTime1]').prop("checked", "checked");
+            $('#newSettingTime input[id^=setTime]').checkboxradio("refresh");
+            selectTime = {};
+            $('label[for^=setTime2]').text('指定時段');
+            $('#floorDefault div').addClass('ui-btn-active');
+            $.each(seqClick, function(index, value) {
+                $('#newSettingFloor div[id=' + value + '] > div').removeClass('ui-btn-active');
+            });
+            seqClick = [];
+            $('#newSettingFloor div[id^=cntIcon]').remove();
+        }
+
         /********************************** page event *************************************/
         $('#viewParkingDetailAdd').one('pagebeforeshow', function(event, ui) {
 
@@ -9,6 +30,13 @@ $("#viewParkingDetailAdd").pagecontainer({
         $('#viewParkingDetailAdd').on('pageshow', function(event, ui) {
   			loadingMask("hide");
   			footerFixed();
+        });
+
+        /********************************** dom event *************************************/
+        $('#newSettingBack').on('click', function() {
+            //setDefaultStatus();
+            clickEditSettingID = '';
+            $.mobile.changePage('#viewMain');
         });
     }
 });
