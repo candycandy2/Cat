cacc4575fb648cb8fee9aace0561fb20a5e4ab15
CleoWChan <Cleo.W.Chan@BenQ.com>
2017-10-18 17:39:35 +0800

    add getQUserDetail

--- API/QChat/app/Http/Controllers/UserController.php
+++ API/QChat/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use Validator;
+use App\lib\Verify;
 use App\lib\ResultCode;
 use Illuminate\Support\Facades\Input;
 use App\Services\UserService;
@@ -30,9 +31,6 @@ class UserController extends Controller
      * 透過此API可以設定人員的詳細資料
      */
     public function setQUserDetail(){
-
-        \DB::beginTransaction();
-
         try {
             if(!isset($this->data['memo'])){
                 $this->data['memo'] = null;
@@ -52,15 +50,51 @@ class UserController extends Controller
             $memo = $this->data['memo'];
             $data = array('memo'=>$memo);
             $res = $this->userService->setQUserDetail($empNo, $data, $userId);
-            \DB::commit();
             $result = ['ResultCode'=>ResultCode::_1_reponseSuccessful,'Message'=>''];
             return response()->json($result);
         }catch (\Exception $e) {
-
-             \DB::rollBack();
-
             $result = ['ResultCode'=>ResultCode::_025999_UnknownError,'Message'=>""];
             return response()->json($result);
         }
     }
+
+    /**
+     * 透過此API可以獲得人員的詳細資訊, 包含分機號碼等
+     */
+    public function getQUserDetail(){
+        try {
+
+            $required = Validator::make($this->data, [
+                'destination_emp_no' => 'required',
+            ]);
+
+            if($required->fails())
+            {
+                return $result = response()->json(['ResultCode'=>ResultCode::_025903_MandatoryFieldLost,
+                        'Message'=>"必填字段缺失",
+                        'Content'=>""]);
+            }
+            $empNo = $this->data['emp_no'];
+            $destinationEmpNo = $this->data['destination_emp_no'];
+            
+            if(!Verify::checkUserStatusByUserEmpNo($destinationEmpNo)) {
+                 return $result = response()->json(['ResultCode'=>ResultCode::_025921_DestinationEmployeeNumberIsInvalid,
+                        'Message'=>"要查詢的人員工號不存在",
+                        'Content'=>""]);
+            }
+
+            $userDerail = $this->userService->getQUserDetail($destinationEmpNo);
+            if($userDerail ==null || count($userDerail) == 0){
+                 return $result = response()->json(['ResultCode'=>ResultCode::_025998_NoData,
+                        'Message'=>"查無資料",
+                        'Content'=>""]);
+            }
+            $result = ['ResultCode'=>ResultCode::_1_reponseSuccessful,'Content'=>$userDerail,'Message'=>''];
+            return response()->json($result);
+        }catch (\Exception $e) {
+            $result = ['ResultCode'=>ResultCode::_025999_UnknownError,'Message'=>""];
+            return response()->json($result);
+        }
+    }
+
 }
