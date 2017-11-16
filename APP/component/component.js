f5f3b044d96c9e7be08ffef0ebb6efc8e5672cd6
alan tu <faulfish@gmail.com>
2017-11-16 08:50:56 +0800

    support iOS 11

--- APP/component/component.js
+++ APP/component/component.js
@@ -375,8 +375,11 @@ $(document).one("pagebeforecreate", function() {
             }
             $("body, input, select, textarea, button, .ui-btn").css("font-family", "Microsoft JhengHei");
         } else if (device.platform === "iOS") {
-            $('.page-header').addClass('ios-fix-overlap');
-            $('.ios-fix-overlap-div').css('display', 'block');
+            if (versionCompare(device.version, "11.0", "") === 1) {
+            } else {
+                $('.page-header').addClass('ios-fix-overlap');
+                $('.ios-fix-overlap-div').css('display', 'block');
+            }
             $('.ui-page:not(#viewInitial)').addClass('ui-page-ios');
             $("body, input, select, textarea, button, .ui-btn").css("font-family", "Heiti TC");
         }
@@ -817,8 +820,11 @@ function setWhiteList() {
         }
 
         if (device.platform === "iOS") {
-            $('.page-header').addClass('ios-fix-overlap');
-            $('.ios-fix-overlap-div').css('display', 'block');
+            if (versionCompare(device.version, "11.0", "") === 1) {
+            } else {
+                $('.page-header').addClass('ios-fix-overlap');
+                $('.ios-fix-overlap-div').css('display', 'block');
+            }
         }
     };
 
@@ -1110,4 +1116,49 @@ function handleOpenURL(url) {
         }
     }
 
+}
+
+function versionCompare(v1, v2, options) {
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
+
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+    }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        } else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
 }
