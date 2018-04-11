49ed9e19d02bc6f1f3eddc84a485273fa18803e8
jenniferywang <jennifer.y.wang@benq.com>
2018-04-11 17:45:30 +0800

    [Insurance] Add InsuranceInfo click link + Revise viewContact & viewInsuranceInfo  UI

--- APP/Insurance/www/js/viewInsuranceInfo.js
+++ APP/Insurance/www/js/viewInsuranceInfo.js
@@ -13,11 +13,25 @@ $("#viewInsuranceInfo").pagecontainer({
 
         $("#viewInsuranceInfo").on("pageshow", function(event, ui) {
             loadingMask("hide");
-
         });
 
         /********************************** dom event *************************************/
-       
+        
+        $("#howtobuy").on('click', function() {
+            window.open("http://blog.sina.com.tw/46272/article.php?entryid=574630", '_system');          
+        });   
+
+        $("#wrongsense").on('click', function() {
+            window.open("http://blog.sina.com.tw/46272/article.php?entryid=574630", '_system');          
+        }); 
+
+        $("#booklist").on('click', function() {
+            window.open("http://blog.sina.com.tw/46272/article.php?entryid=574630", '_system');          
+        });  
+
+        $("#QA").on('click', function() {
+            window.open("http://blog.sina.com.tw/46272/article.php?entryid=574630", '_system');          
+        });            
     }
 });
 
