d6ee6136837bb6cb9499689643828cbc1e792084
jenniferywang <jennifer.y.wang@benq.com>
2018-05-04 18:31:12 +0800

    [Insurance] GroupInsurance AddFakeData and Reivse UI in empty/full Data

--- APP/Insurance/www/js/viewPersonalInsurance.js
+++ APP/Insurance/www/js/viewPersonalInsurance.js
@@ -2,7 +2,12 @@ var healthInsurArr = [{emplid:"1607001", INS_ID:"", APP_ID:"", family_id:"1", na
                       {emplid:"1607001", INS_ID:"", APP_ID:"", family_id:"2", name:"王小美", relation:"父母", birthday:"1980/01/02", idno:"A223456788", group:"眷屬健保在保", insuredday:"2017-01-01", reason:"", subsidy:"1", certificate:"Y", healthcard:"Y", applyday:"2017-01-01", dealwith:"已加保", dealwithday:"2017-01-01", remark:"", can_apply:"停保+退保"},
                       {emplid:"1607001", INS_ID:"", APP_ID:"", family_id:"3", name:"王晶晶", relation:"祖父母", birthday:"1950/01/02", idno:"A223456888", group:"眷屬健保不在保", insuredday:"", reason:"", subsidy:"5", certificate:"N", healthcard:"Y", applyday:"2017-01-01", dealwith:"", dealwithday:"", remark:"", can_apply:"加保"},
                       {emplid:"1607001", INS_ID:"", APP_ID:"", family_id:"4", name:"王囧囧", relation:"祖父母", birthday:"1950/01/01", idno:"A123456889", group:"眷屬健保不在保", insuredday:"", reason:"", subsidy:"5", certificate:"N", healthcard:"Y", applyday:"2017-01-01", dealwith:"", dealwithday:"", remark:"", can_apply:"加保"}];
-var groupInsurArr =  "";
+/*var groupInsurArr = [{emplid:"1607001", name:"林偉人", goupinsurancetype:"自費一般險(南山)", groupinsurancefun:"本人自費一般險A方案", insuredday:"2017-10-01", status:"生效"},
+                     {emplid:"1607001", name:"林小明", goupinsurancetype:"自費一般險(南山)", groupinsurancefun:"祖父母自費一般險A方案", insuredday:"2017-10-01", status:"生效"},
+                     {emplid:"1607001", name:"林偉人", goupinsurancetype:"自費意外險(南山)", groupinsurancefun:"本人自費一般險B方案", insuredday:"2017-10-01", status:"生效"},
+                     {emplid:"1607001", name:"林小明", goupinsurancetype:"自費意外險(南山)", groupinsurancefun:"祖父母自費一般險B方案", insuredday:"2017-10-01", status:"生效"}];*/
+var groupInsurArr = [{emplid:"1607001", name:"林偉人", goupinsurancetype:"自費意外險(南山)", groupinsurancefun:"本人自費一般險B方案", insuredday:"2017-10-01", status:"生效"},
+                     {emplid:"1607001", name:"林小明", goupinsurancetype:"自費意外險(南山)", groupinsurancefun:"祖父母自費一般險B方案", insuredday:"2017-10-01", status:"生效"}];
 
 $("#viewPersonalInsurance").pagecontainer({
     create: function(event, ui) {
@@ -29,22 +34,72 @@ $("#viewPersonalInsurance").pagecontainer({
 
         //API:QueryGroupInsuranceData
         function queryGroupInsuranceList() {
+            loadingMask("show");
             if (groupInsurArr == ""){
-                var groupInsurList = '<div class="empty-list"><div>無</div></div>';               
-                $("#normalInsur").empty().append(groupInsurList);
-                $("#accidentInsur").empty().append(groupInsurList);
+                var normalInsurList = '<div class="empty-list"><div>無</div></div>';               
+                $("#normalInsur").empty().append(normalInsurList);
+                $("#accidentInsur").empty().append(normalInsurList);
+            }else {
+                var normalInsurList = ""; 
+                var accidentInsurList = "";
+                var isNormalInsur = false;
+                var isAccidentInsur = false;
+                
+                for (var i=0; i<groupInsurArr.length; i++ ) {
+                    var groupInsurType = groupInsurArr[i]["goupinsurancetype"];
+                    if (groupInsurType.substr(0,5) == "自費一般險"){
+                        normalInsurList += '<div class="group-list"><div><span>'
+                        + groupInsurArr[i]["name"]
+                        + '</span>/<span>'
+                        + groupInsurArr[i]["groupinsurancefun"]
+                        + '</span></div><div><span>'
+                        + groupInsurArr[i]["insuredday"]
+                        + '</span> <span>' 
+                        + groupInsurArr[i]["status"]
+                        + '</span></div></div><div class="activity-line"></div>';
+                        isNormalInsur = true;
+                    }  
+                    if (groupInsurType.substr(0,5) == "自費意外險"){
+                        accidentInsurList += '<div class="group-list"><div><span>'
+                        + groupInsurArr[i]["name"]
+                        + '</span>/<span>'
+                        + groupInsurArr[i]["groupinsurancefun"]
+                        + '</span></div><div><span>'
+                        + groupInsurArr[i]["insuredday"]
+                        + '</span> <span>' 
+                        + groupInsurArr[i]["status"]
+                        + '</span></div></div><div class="activity-line"></div>';
+                        isAccidentInsur = true;
+                    }                     
+                }
+                if (isNormalInsur == true){
+                    $("#normalInsur").empty().append(normalInsurList).children("div:last-child").remove();
+                }else if (isNormalInsur == false){
+                    var normalInsurList = '<div class="empty-list"><div>無</div></div>';               
+                    $("#normalInsur").empty().append(normalInsurList);
+                }
+                if (isAccidentInsur == true){
+                    $("#accidentInsur").empty().append(accidentInsurList).children("div:last-child").remove();
+                }else if (isAccidentInsur == false){
+                    var accidentInsurList = '<div class="empty-list"><div>無</div></div>';               
+                    $("#accidentInsur").empty().append(accidentInsurList);
+                }
             }
+            loadingMask("hide");
         }
 
         //API:QueryHealthInsuranceFamily  
         function queryHealthInsuranceList() {
+            loadingMask("show");
             if (healthInsurArr == ""){
                 var healthInsurList = '<div class="empty-list"><div>無</div></div>';                
                 $("#inHealthInsur").empty().append(healthInsurList);
                 $("#nonHealthInsur").empty().append(healthInsurList);
             }else {
                 var healthInsurList = ""; 
-                var nonhealthInsurList = "";
+                var nonHealthInsurList = "";
+                var inInsur= false;
+                var notInsur = false;
                 for (var i=0; i<healthInsurArr.length; i++ ) {
                     if (healthInsurArr[i]["group"] == "眷屬健保在保"){                        
                         healthInsurList += '<div class="family-list"><div data-id="'
@@ -58,9 +113,10 @@ $("#viewPersonalInsurance").pagecontainer({
                         + '</div><div>'
                         + healthInsurArr[i]["idno"]
                         + '</div></div><div><img src="img/100_btn_nextpage.png" class="family-next"></div></div><div class="activity-line"></div>';
+                        inInsur = true;
                     }
                     if (healthInsurArr[i]["group"] == "眷屬健保不在保"){
-                        nonhealthInsurList += '<div class="family-list"><div data-id="'
+                        nonHealthInsurList += '<div class="family-list"><div data-id="'
                         + healthInsurArr[i]["family_id"]
                         + '"><div><span>'
                         + healthInsurArr[i]["name"]
@@ -71,10 +127,22 @@ $("#viewPersonalInsurance").pagecontainer({
                         + '</div><div>'
                         + healthInsurArr[i]["idno"]
                         + '</div></div><div><img src="img/024_btn_addfriend.png" class="family-add"></div></div><div class="activity-line"></div>';
+                        notInsur = true;
                     }
                 }
-                $("#inHealthInsur").empty().append(healthInsurList).children("div:last-child").remove();
-                $("#nonHealthInsur").empty().append(nonhealthInsurList).children("div:last-child").remove();
+                if (inInsur == true){
+                    $("#inHealthInsur").empty().append(healthInsurList).children("div:last-child").remove();
+                }else if (inInsur == false){
+                    var healthInsurList = '<div class="empty-list"><div>無</div></div>';               
+                    $("#inHealthInsur").empty().append(healthInsurList);
+                }
+                if (notInsur == true){
+                    $("#nonHealthInsur").empty().append(nonHealthInsurList).children("div:last-child").remove();
+                }else if (notInsur == false){
+                     var nonHealthInsurList = '<div class="empty-list"><div>無</div></div>';               
+                    $("#nonHealthInsur").empty().append(nonHealthInsurList);
+                }                
+                
             }
             loadingMask("hide");
         }
@@ -84,13 +152,12 @@ $("#viewPersonalInsurance").pagecontainer({
             $('#pageInsurStatus-1').show();
             $('#pageInsurStatus-2').hide();
             $("label[for=fam-insur-tab-2]").removeClass('ui-btn-active');
-            $("label[for=fam-insur-tab-1]").addClass('ui-btn-active');
-            queryHealthInsuranceList();
-            queryGroupInsuranceList();  
+            $("label[for=fam-insur-tab-1]").addClass('ui-btn-active');            
         });
 
-        $("#viewPersonalInsurance").on("pageshow", function(event, ui) {       
-            
+        $("#viewPersonalInsurance").on("pageshow", function(event, ui) {  
+            queryHealthInsuranceList();
+            queryGroupInsuranceList();                 
         });
 
         /********************************** dom event *************************************/
