3b688df25ecaac441a366b157150dc9f7a49feb5
CleoWChan <Cleo.W.Chan@BenQ.com>
2018-07-06 18:22:26 +0800

    [QPlayAPI] fix sendMail only allow http post to call, and verify parameter

--- qplayApi/qplayApi/app/Http/Controllers/mailController.php
+++ qplayApi/qplayApi/app/Http/Controllers/mailController.php
@@ -23,11 +23,35 @@ class mailController extends Controller
         $Verify = new Verify();
         $verifyResult = $Verify->verifyCustom(false);
         if($verifyResult["code"] == ResultCode::_1_reponseSuccessful)
-        {
+        {   
+
+            if(is_null($request->json('from')) || trim($request->json('from')) == ""
+            ||!filter_var($request->json('from'), FILTER_VALIDATE_EMAIL)
+            || is_null($request->json('from_name')) || trim($request->json('from_name')) == ""
+            || is_null($request->json('to')) || trim($request->json('to')) == ""
+            || is_null($request->json('subject')) || trim($request->json('subject')) == ""
+            || is_null($request->json('content')))
+            {
+               
+                 $result = ['result_code'=>ResultCode::_999001_requestParameterLostOrIncorrect,
+                    'message'=>CommonUtil::getMessageContentByCode(ResultCode::_999001_requestParameterLostOrIncorrect),
+                    'content'=>''];
+                return response()->json($result);
+            }
+            $revicer = explode(",", $request->json('to'));
+            foreach ($revicer as $email) {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                  $result = ['result_code'=>ResultCode::_999001_requestParameterLostOrIncorrect,
+                    'message'=>CommonUtil::getMessageContentByCode(ResultCode::_999001_requestParameterLostOrIncorrect),
+                    'content'=>''];
+                return response()->json($result);
+                }
+            }
+
             Mail::raw($request->json('content'), function ($message) use($request) {
                 $from = $request->json('from');
                 $fromName = $request->json('from_name');
-                $to = $request->json('to');
+                $to = explode(",", $request->json('to'));
                 $subject = $request->json('subject');
                 $message->from($from, $fromName);
                 $message->to($to)->subject($subject);
