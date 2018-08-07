a23a0dfd3b204a118fa06b2a07eee5552d6e6590
AllenZheYuan <Allen.Z.Yuan@Qisda.com>
2018-08-07 10:40:29 +0800

    [component] review viewNewsEvents to viewMessageList

--- APP/component/component.js
+++ APP/component/component.js
@@ -231,7 +231,9 @@ var app = {
                 //Before open Message Detail Data, update Message List
                 if (window.localStorage.getItem("msgDateFrom") === null) {
 
-                    $.mobile.changePage('#viewNewsEvents2-3');
+					//review by allen
+                    //$.mobile.changePage('#viewNewsEvents2-3');
+					$.mobile.changePage('#viewMessageList');
                 } else {
                     if (window.localStorage.getItem("uuid") !== null) {
                         loginData["uuid"] = window.localStorage.getItem("uuid");
