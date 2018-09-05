a7a44ccf2a4ce213bdcebfd35e14e198b9deedea
alan tu <faulfish@gmail.com>
2018-09-05 16:40:44 +0800

    remove unused page and variable

--- APP/QPlayV3.0/www/js/index.js
+++ APP/QPlayV3.0/www/js/index.js
@@ -1,7 +1,7 @@
 /*global variable*/
 var appKeyOriginal = "appqplay";
 var appKey = "appqplay";
-var pageList = ["viewMain2-1", "viewAppDetail2-2", "viewNewsEvents2-3", "viewWebNews2-3-1", "viewMain3"];
+var pageList = ["viewMain3"];
 var pageVisitedList = ["viewMain3"];
 var appSecretKey = "swexuc453refebraXecujeruBraqAc4e";
 
@@ -34,7 +34,7 @@ var reserveCalendar = null;
 var portalURL = "",
     messageFrom = 'viewMain3';
 
-window.initialSuccess = function (data) {
+window.initialSuccess = function(data) {
     //1. widgetlist
     checkWidgetListOrder();
 
@@ -98,7 +98,7 @@ function checkWidgetListOrder() {
     if (widgetArr == null) {
         //1. 如果local没有数据，直接获取widget.js
         var widget_arr = widget.list();
-        
+
         //2. 以widget.js为主遍历FunctionList，如果任何一个为不可用，则enabled为false
         var widgetObj = compareWidgetAndFunction(widget_arr, widget_list);
 
@@ -177,7 +177,7 @@ function compareWidgetAndFunction(wdgArr, funArr) {
 
 //先按照开始时间排序，如果开始时间一致再用结束时间排序
 function sortByBeginTime(prop1, prop2) {
-    return function (obj1, obj2) {
+    return function(obj1, obj2) {
         var val1 = obj1[prop1].replace(':', '');
         var val2 = obj2[prop1].replace(':', '');
         var value1 = obj1[prop2].replace(':', '');
@@ -203,11 +203,11 @@ function sendPushToken() {
     var self = this;
     var queryStr = "&app_key=" + qplayAppKey + "&device_type=" + loginData.deviceType;
 
-    this.successCallback = function () { };
+    this.successCallback = function() {};
 
-    this.failCallback = function () { };
+    this.failCallback = function() {};
 
-    var __construct = function () {
+    var __construct = function() {
         if (loginData.token !== null && loginData.token.length !== 0) {
             QPlayAPI("POST", "sendPushToken", self.successCallback, self.failCallback, null, queryStr);
         }
@@ -218,7 +218,7 @@ function sendPushToken() {
 function reNewToken() {
     var self = this;
 
-    this.successCallback = function (data) {
+    this.successCallback = function(data) {
         var resultcode = data['result_code'];
         var newToken = data['content'].token;
         var newTokenValid = data['token_valid'];
@@ -239,9 +239,9 @@ function reNewToken() {
         //}
     };
 
-    this.failCallback = function (data) { };
+    this.failCallback = function(data) {};
 
-    var __construct = function () {
+    var __construct = function() {
         QPlayAPI("POST", "renewToken", self.successCallback, self.failCallback, null, null);
     }();
 }
@@ -329,13 +329,13 @@ function unregister() {
     var self = this;
     var queryStr = "&target_uuid=" + loginData.uuid;
 
-    this.successCallback = function (data) {
+    this.successCallback = function(data) {
         console.log(data);
     };
 
-    this.failCallback = function (data) { };
+    this.failCallback = function(data) {};
 
-    var __construct = function () {
+    var __construct = function() {
         QPlayAPI("POST", "unregister", self.successCallback, self.failCallback, null, queryStr);
     }();
 }
@@ -343,25 +343,25 @@ function unregister() {
 function addDownloadHit(appname) {
     var self = this;
 
-    this.successCallback = function (data) {
+    this.successCallback = function(data) {
         var resultcode = data['result_code'];
 
-        if (resultcode == 1) { } else { }
+        if (resultcode == 1) {} else {}
     };
 
-    this.failCallback = function (data) {
+    this.failCallback = function(data) {
         var resultcode = data['result_code'];
 
-        if (resultcode == 1) { } else { }
+        if (resultcode == 1) {} else {}
     };
 
-    var __construct = function () {
+    var __construct = function() {
         var queryStr = "&login_id=" + loginData.loginid + "&package_name=" + appname;
         QPlayAPI("GET", "addDownloadHit", self.successCallback, self.failCallback, null, queryStr);
     }();
 }
 
-Date.prototype.FormatReleaseDate = function () {
+Date.prototype.FormatReleaseDate = function() {
     return this.getFullYear() + "年" + (parseInt(this.getMonth()) + 1) + "月" + this.getDate() + "日";
 }
 
@@ -376,7 +376,7 @@ function scrollLeftOffset(margin) {
 }
 
 //Change event type
-$(document).on("click", ".event-type", function () {
+$(document).on("click", ".event-type", function() {
     $("#eventTypeSelect").panel("open");
 });
 
@@ -405,6 +405,6 @@ function onBackKeyDown() {
 }
 
 //header区域返回button
-$(document).on('click', '.page-back', function () {
+$(document).on('click', '.page-back', function() {
     onBackKeyDown();
 })
