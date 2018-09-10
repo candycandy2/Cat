90fe8e9b32c8b4b9d36207dedc47f6ca28d05df3
alan tu <faulfish@gmail.com>
2018-09-10 13:42:29 +0800

    refectory - getFunctionList()

--- APP/component/function/clearFunctiuonList.js
+++
@@ -1,9 +0,0 @@
-
-//清空FunctionList
-function clearFunctionList() {
-    var function_list = window.localStorage.getItem('FunctionList');
-
-    if(function_list !== null) {
-        window.localStorage.removeItem('FunctionList');
-    }
-}
