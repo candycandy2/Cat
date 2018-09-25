6cab81e39958e3842bdccd82ca89ef6cade5577c
AllenZheYuan <Allen.Z.Yuan@Qisda.com>
2018-09-25 11:32:08 +0800

    [QPlay] modify applist open other app

--- WEB/widget/widgetPage/viewAppList/viewAppList.js
+++ WEB/widget/widgetPage/viewAppList/viewAppList.js
@@ -85,15 +85,18 @@ $("#viewAppList").pagecontainer({
                 var appurlicon = applist[notDownloadList[i]].icon_url;
                 var appcode = applist[notDownloadList[i]].app_code;
 
-                notdownloadContent += '<div class="download-list"><div class="download-link" data-code="' +
-                    appcode +
-                    '"><div class="download-icon"><img src="' +
-                    appurlicon +
-                    '"></div><div class="download-name"><div class="font-style10">' +
-                    packagename +
-                    '</div><div class="font-style7">' +
-                    appSummary +
-                    '</div></div></div><div><img src="img/download_icon.png" class="download-btn" data-src="download_icon"></div></div>';
+                if (packagename.includes('Gaia',0) === false && packagename.includes('Learning',0) === false && packagename.includes('無人搬運',0) === false) {
+                    notdownloadContent += '<div class="download-list"><div class="download-link" data-code="' +
+                        appcode +
+                        '"><div class="download-icon"><img src="' +
+                        appurlicon +
+                        '"></div><div class="download-name"><div class="font-style10">' +
+                        packagename +
+                        '</div><div class="font-style7">' +
+                        appSummary +
+                        '</div></div></div><div><img src="img/download_icon.png" class="download-btn" data-src="download_icon"></div></div>';
+                }
+                
             }
 
             $('.not-download-list').html('').append(notdownloadContent);
