56fc006488ea2c19d235e13297a1c49664d36d10
AllenZheYuan <Allen.Z.Yuan@Qisda.com>
2018-10-18 15:14:14 +0800

    [component] add function get user phoneBook

---
+++ APP/component/function/getMyPhoneBook.js
@@ -0,0 +1,16 @@
+
+//CallAPI get applist
+function getMyPhoneBook(key, secret) {
+    var self = this;
+    var queryData = '<LayoutHeader><User_EmpID>' + loginData["emp_no"] + '</User_EmpID></LayoutHeader>';
+
+    this.successCallback = function(data) {
+        window.sessionStorage.setItem('myPhoneBookList', JSON.stringify(data));
+    };
+
+    this.failCallback = function() {};
+
+    var __construct = function() {
+        CustomAPIByKey("POST", true, key, secret, "QueryMyPhoneBook", self.successCallback, self.failCallback, queryData, "", 60 * 60, "low");
+    }();
+}
