71d5b5c836caa503045bad84d234efe5ebef0965
AllenZheYuan <Allen.Z.Yuan@Qisda.com>
2018-10-19 15:08:15 +0800

    [widget] update yellowpage widget show more telephone

--- WEB/widget/yellowpage/yellowpage.js
+++ WEB/widget/yellowpage/yellowpage.js
@@ -5,17 +5,32 @@ var yellowpageWidget = {
     yellowPageSecretKey: 'c103dd9568f8493187e02d4680e1bf2f',
 
     init: function(contentItem) {
+        var _key = this.yellowPageKey;
 
-        function createContent() {
+        function createContent(key) {
             $.get(serverURL + "/widget/yellowpage/yellowpage.html", function(data) {
                 //1.html
                 contentItem.html('').append(data);
                 //2.img
                 var rateImg = $('<img>').attr('src', serverURL + '/widget/yellowpage/img/widget_yellowpage.png');
                 $('.yellowpage-widget-icon').html('').append(rateImg);
-                
+                var moreImg = $('<img>').attr('src', serverURL + '/widget/yellowpage/img/more_green.png');
+                $('.phone-book-more').html('').append(moreImg);
 
             }, "html");
+
+            //点击更多，跳转YellowPage APP
+            contentItem.on('click', '.phone-book-more', function() {
+                var env = '';
+                if (loginData["versionName"].indexOf("Staging") !== -1) {
+                    env = 'test';
+                } else if (loginData["versionName"].indexOf("Development") !== -1) {
+                    env = 'dev';
+                }
+
+                var schemeURL = key + env + createAPPSchemeURL();
+                openAPP(schemeURL);
+            });
 
         }
 
@@ -35,7 +50,7 @@ var yellowpageWidget = {
                     });
                 }
 
-                createContent();
+                createContent(_key);
 
             });
         }
@@ -46,7 +61,7 @@ var yellowpageWidget = {
             },
             refresh: function(jq) {
                 return jq.each(function() {
-                    createContent();
+                    createContent(_key);
                 });
             }
         }
@@ -60,23 +75,75 @@ var yellowpageWidget = {
         var queryData = '<LayoutHeader><User_EmpID>' + loginData["emp_no"] + '</User_EmpID></LayoutHeader>';
 
         this.successCallback = function(data) {
+            //init
+            $('.phone-book-ul').html('');
+            //result code
             var resultCode = data['ResultCode'];
             if(resultCode == '1') {
                 $('.no-phone-book').hide();
                 //book list
                 var phoneList = data['Content'];
-                var content = '';
+                //var content = '';
                 for(var i in phoneList) {
                     if(i < 3) {
-                        var extNo = $.trim(phoneList[i]['Ext_No']).split(';')[0];
+                        //merge tel & vpn
+                        var telArr = ($.trim(phoneList[i]['Ext_No']) + ';' + $.trim(phoneList[i]['Mvpn'])).split(';');
+                        var arr = [];
+                        for(var j in telArr) {
+                            if(telArr[j] != '') {
+                                arr.push(telArr[j]);
+                            }
+                        }
 
-                        content += '<li class="phone-book-li"><div class="phone-book-name"><div>' + $.trim(phoneList[i]['Name_EN']) +
-                            '</div><div>' + $.trim(phoneList[i]['Name_CH']) + '</div></div><div><img src="' + serverURL +
-                            '/widget/yellowpage/img/phone.png"></div><div>' + extNo + '</div><div><img src="' +
-                            serverURL + '/widget/yellowpage/img/info.png"></div></li>';
+                        var content = '<li class="phone-book-li" data-index="' + i + '"><div class="phone-book-name"><div>' + $.trim(phoneList[i]['Name_EN']) +
+                            '</div><div>' + $.trim(phoneList[i]['Name_CH']) + '</div></div><div><a href="' + (arr.length == 1 ? 'tel:' + arr[0] : '#') +
+                            '"><img src="' + serverURL + '/widget/yellowpage/img/phone.png"></a></div><div><a href="' + (arr.length == 1 ? 'tel:' + arr[0] : '#') +
+                            '">' + (typeof arr[0] != 'undefined' ? arr[0] : '') + '</a></div><div><img src="' + serverURL + '/widget/yellowpage/img/info.png" class="open-yellowpage"></div></li>';
+
+                        $('.phone-book-ul').append(content);
+                        //bind data
+                        $('.phone-book-li[data-index="' + i + '"]').data('tel', arr);
+                        
                     }
                 }
-                $('.phone-book-ul').html('').append(content).show();
+                $('.phone-book-ul').show();
+
+                //bind a href='#' click
+                $('.phone-book-ul').on('click', 'a[href="#"]' ,function() {
+                    //get bind data
+                    var telArr = $(this).parents('.phone-book-li').data('tel');
+                    if(telArr.length > 0) {
+                        //append html
+                        var content = '';
+                        for(var i in telArr) {
+                            content += '<li><img src="' + serverURL + '/widget/yellowpage/img/phone.png" style="width:4vw; height:auto;"><span>' +
+                                '<a href="tel:' + telArr[i] + '" style="text-decoration: none; font-weight: normal;">' + telArr[i] + '</a></span></li>';
+                        }
+                        $('#numSelectPopupWindow ul').html('').append(content);
+                        //open popup
+                        $('#numSelectPopupWindow').popup();
+                        $('#numSelectPopupWindow').show();
+                        $('#numSelectPopupWindow').popup('open');
+                    }
+                });
+
+                //close popup
+                $('#numPopupCloseBtn').on('click', function(){
+                    $('#numSelectPopupWindow').popup('close');
+                });
+
+                //bind open yellow page app
+                $('.phone-book-ul').on('click', '.open-yellowpage', function() {
+                    var env = '';
+                    if (loginData["versionName"].indexOf("Staging") !== -1) {
+                        env = 'test';
+                    } else if (loginData["versionName"].indexOf("Development") !== -1) {
+                        env = 'dev';
+                    }
+
+                    var schemeURL = key + env + createAPPSchemeURL();
+                    openAPP(schemeURL);
+                });
 
             } else {
                 $('.no-phone-book').show();
