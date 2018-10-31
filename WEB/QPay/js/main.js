6c0e4e1fdf69ea60829d8d71721674e6fd32326a
Darren Ti <Darren.K.Ti@BenQ.com>
2018-10-31 16:23:21 +0800

    [QPayWebAPP]Fix Bug for check DatePicker duration

--- WEB/QPay/js/main.js
+++ WEB/QPay/js/main.js
@@ -19,25 +19,58 @@ $(function() {
         browserLanguage = "en-us";
     }
 
+    //Date Picker
+    $(".date-picker").datepicker({
+        dateFormat: "yy/mm/dd",
+        autoSize: false,
+        beforeShow: function() {
+            var screenWidth = document.documentElement.clientWidth;
+            var datePickerWidth = $(".ui-datepicker").width();
+            var positionLeft = parseInt((screenWidth - datePickerWidth) / 2, 10);
+            $(".ui-datepicker").css("opacity", "0");
+
+            setTimeout(function() {
+                $(".mask").show();
+
+                $(".ui-datepicker").css({
+                    "opacity": "1",
+                    "left": positionLeft + "px",
+                    "z-index": 1001
+                });
+            }, 300);
+        },
+        onClose: function(dateText, inst) {
+            $(".mask").hide();
+
+            var startTimestamp = parseInt(new Date($("#startDate").val() + " 00:00:00").getTime() / 1000, 10);
+            var endTimestamp = parseInt(new Date($("#endDate").val() + " 23:59:59").getTime() / 1000, 10);
+
+            if (endTimestamp < startTimestamp) {
+                if (inst.id === "startDate") {
+                    var startDate = $("#startDate").datepicker("getDate");
+                    var endDate = new Date(startDate.setDate(startDate.getDate() + 7));
+                    //var endDateStr = endDate.getFullYear() + "/" + padLeft(parseInt(endDate.getMonth() + 1, 10), 2) + "/" + padLeft(endDate.getUTCDate(), 2);
+                    var endDateStr = formatDate(endDate);
+
+                    $("#endDate").datepicker("setDate", endDateStr);
+                } else {
+                    var endDate = $("#endDate").datepicker("getDate");
+                    var startDate = new Date(endDate.setDate(endDate.getDate() - 5));
+                    //var startDateStr = startDate.getFullYear() + "/" + padLeft(parseInt(startDate.getMonth() + 1, 10), 2) + "/" + padLeft(startDate.getUTCDate(), 2);
+                    var startDateStr = formatDate(startDate);
+
+                    $("#startDate").datepicker("setDate", startDateStr);
+                }
+            }
+        }
+    });
+
     //------------------------General Function------------------------
     window.mobilecheck = function() {
       var check = false;
       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
       return check;
     };
-
-    function initialAPP() {
-        //Do not remember input data
-        $("input").val("");
-
-        var isMobile = window.mobilecheck();
-        if (isMobile) {
-            $("#viewRecord .record-content .record-body > .record").html("");
-        } else {
-            $("#viewRecord .record-content record-table.desktop tbody > tr").html("");
-        }
-    }
-    initialAPP();
 
     function callAPI(headerData, requestAction, successCallback, failCallback, queryStr) {
         queryStr = queryStr || "";
@@ -86,6 +119,10 @@ $(function() {
         return fixedDateStr.getFullYear() + "/" + padLeft(parseInt(fixedDateStr.getMonth() + 1, 10), 2) + "/" + padLeft(fixedDateStr.getUTCDate(), 2) + " " +
             padLeft(fixedDateStr.getHours(), 2) + ":" + padLeft(fixedDateStr.getMinutes(), 2) + ":" + padLeft(fixedDateStr.getSeconds(), 2);
     };
+
+    function formatDate(date) {
+        return date.getFullYear() + "/" + padLeft(parseInt(date.getMonth() + 1, 10), 2) + "/" + padLeft(date.getUTCDate(), 2);
+    }
 
     function displayDatetime(timestamp) {
         var datetime = new Date();
@@ -155,6 +192,29 @@ $(function() {
         changePage("viewLogin");
     }
 
+    function initialAPP() {
+        //Do not remember input data
+        $("input").val("");
+
+        var isMobile = window.mobilecheck();
+        if (isMobile) {
+            $("#viewRecord .record-content .record-body > .record").remove();
+        } else {
+            $("#viewRecord .record-content .record-table.desktop tbody > tr").remove();
+        }
+
+        //Set Datepicker
+        var todayDate = new Date();
+        var endDateStr = formatDate(todayDate);
+
+        var startDate = new Date(todayDate.setDate(todayDate.getDate() - 6));
+        var startDateStr = formatDate(startDate);
+
+        $("#startDate").datepicker("setDate", startDateStr);
+        $("#endDate").datepicker("setDate", endDateStr);
+    }
+    initialAPP();
+
     //------------------------General Event------------------------
     if (window.mobilecheck()) {
         var screenHeight = document.documentElement.clientHeight;
@@ -196,38 +256,14 @@ $(function() {
 
         //Page
         $(".page").each(function(index, dom) {
-            if ($(dom).prop("id") != "viewRecord") {
-                $(dom).css("min-height", document.documentElement.clientHeight + "px");
-            }
+            $(dom).css({
+                "min-height": document.documentElement.clientHeight + "px",
+                "padding-top": 0
+            });
         });
 
         changePage("viewLogin");
     }
-
-    //Date Picker
-    $(".date-picker").datepicker({
-        dateFormat: "yy/mm/dd",
-        autoSize: false,
-        beforeShow: function() {
-            var screenWidth = document.documentElement.clientWidth;
-            var datePickerWidth = $(".ui-datepicker").width();
-            var positionLeft = parseInt((screenWidth - datePickerWidth) / 2, 10);
-            $(".ui-datepicker").css("opacity", "0");
-
-            setTimeout(function() {
-                $(".mask").show();
-
-                $(".ui-datepicker").css({
-                    "opacity": "1",
-                    "left": positionLeft + "px",
-                    "z-index": 1001
-                });
-            }, 300);
-        },
-        onClose: function() {
-            $(".mask").hide();
-        }
-    });
 
     //Mask
     $(document).on("click", ".mask", function() {
@@ -280,6 +316,7 @@ $(function() {
                 if (resultCode === 1) {
                     $("#empNumber").html(window.loginData["empNO"]);
                     $("#pointNow").html(data["point_now"]);
+
                     window.loginData["signature"] = data["signature"];
                     window.loginData["signatureTime"] = data["signature_time"];
                     window.loginData["token"] = data["token"];
@@ -295,6 +332,7 @@ $(function() {
 
                     initialAPP();
                     changePage("viewRecord");
+                    $("#viewRecord .number-content").hide();
                 } else {
                     message("open", data["message"]);
                 }
@@ -396,6 +434,7 @@ $(function() {
 
                 if (resultCode === 1) {
                     message("open", "交易密碼修改成功", "viewRecord");
+                    $("#viewChangePwd input").val("");
                 } else if (resultCode === "999008") {
                     logout();
                 } else {
@@ -496,45 +535,49 @@ $(function() {
         var isMobile = window.mobilecheck();
 
         if (isMobile) {
-            $("#viewRecord .record-content .record-body > .record").html("");
+            $("#viewRecord .record-content .record-body > .record").remove();
             var recordHTML = $("template#tplRecordMobile").html();
         } else {
-            $("#viewRecord .record-content record-table.desktop tbody > tr").html("");
+            $("#viewRecord .record-content .record-table.desktop tbody > tr").remove();
             var recordHTML = $("template#tplRecordDesktop").html();
         }
 
-        for (var i=0; i<recordData.length; i++) {
-            var record = $(recordHTML);
-            var displayName = "";
-            var pointSymbol = "";
+        if (recordData.length == 0) {
+            $("#viewRecord .number-content").hide();
+        } else {
+            for (var i=0; i<recordData.length; i++) {
+                var record = $(recordHTML);
+                var displayName = "";
+                var pointSymbol = "";
 
-            if (recordData[i]["trade_type"] == "store") {
-                displayName = recordData[i]["point_type_name"];
-                pointSymbol = "+";
-            } else if (recordData[i]["trade_type"] == "trade") {
-                displayName = recordData[i]["shop_name"];
-                pointSymbol = "-";
+                if (recordData[i]["trade_type"] == "store") {
+                    displayName = recordData[i]["point_type_name"];
+                    pointSymbol = "+";
+                } else if (recordData[i]["trade_type"] == "trade") {
+                    displayName = recordData[i]["shop_name"];
+                    pointSymbol = "-";
+
+                    if (isMobile) {
+                        record.find(".column-4 .point-number").removeClass("point-add");
+                    } else {
+                        record.find(".column-4").removeClass("point-add");
+                    }
+                }
+
+                record.find(".column-1 .shop-point").html(displayName);
+                record.find(".column-2").html(recordData[i]["trade_id"]);
+                record.find(".column-3 .datetime").html(displayDatetime(recordData[i]["trade_time"]));
+                record.find(".column-4 .point-number").html(pointSymbol + recordData[i]["trade_point"]);
 
                 if (isMobile) {
-                    record.find(".column-4 .point-number").removeClass("point-add");
+                    $("#viewRecord .record-content .record-body").append(record);
                 } else {
-                    record.find(".column-4").removeClass("point-add");
-                }
-            }
+                    $("#viewRecord .record-content .record-table.desktop tbody").append(record);
 
-            record.find(".column-1 .shop-point").html(displayName);
-            record.find(".column-2").html(recordData[i]["trade_id"]);
-            record.find(".column-3 .datetime").html(displayDatetime(recordData[i]["trade_time"]));
-            record.find(".column-4 .point-number").html(pointSymbol + recordData[i]["trade_point"]);
-
-            if (isMobile) {
-                $("#viewRecord .record-content .record-body").append(record);
-            } else {
-                $("#viewRecord .record-content .record-table.desktop tbody").append(record);
-
-                if ((i + 1) == recordData.length) {
-                    $("#viewRecord .number-content").show();
-                    pageNumberView("prev", 1);
+                    if ((i + 1) == recordData.length) {
+                        $("#viewRecord .number-content").show();
+                        pageNumberView("prev", 1);
+                    }
                 }
             }
         }
@@ -544,7 +587,7 @@ $(function() {
         number = number || null;
 
         var dataCount = $("#viewRecord .record-content .record-table.desktop tbody tr").length;
-        var pageDataNumber = 6;
+        var pageDataNumber = 10;
         var pageTotal = Math.ceil(dataCount / pageDataNumber);
 
         if (action == "prev") {
@@ -608,7 +651,7 @@ $(function() {
 
         if (startDate.length == 0 || endDate.length == 0) {
             message("open", "請選擇日期");
-        } else if (endTimestamp <= startTimestamp) {
+        } else if (endTimestamp < startTimestamp) {
             message("open", "日期區間不正確");
         } else {
             tradeRecordQPayWeb(startTimestamp, endTimestamp);
