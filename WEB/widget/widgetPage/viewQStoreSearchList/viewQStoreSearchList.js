449769647f263fc82407569addca899e194baa08
jenniferywang <jennifer.y.wang@benq.com>
2018-11-23 18:27:27 +0800

    [QStore] Add City and Category DDL

--- WEB/widget/widgetPage/viewQStoreSearchList/viewQStoreSearchList.js
+++ WEB/widget/widgetPage/viewQStoreSearchList/viewQStoreSearchList.js
@@ -9,19 +9,65 @@ $("#viewQStoreSearchList").pagecontainer({
             defaultText: langStr["wgt_108"],
             changeDefaultText: true,
             attr: {
-                class: "ddl-select-img"
+                class: "tpl-dropdown-list-icon-arrow"
             }
         };
-        function getAllCityList() {
+        var categoryData = {
+            id: "category-popup",
+            option: [],
+            title: '',
+            defaultText: langStr["wgt_109"],
+            changeDefaultText: true,
+            attr: {
+                class: "tpl-dropdown-list-icon-arrow"
+            }
+        };
 
+        function getAllCityList() {
+            var  cityList = [ "所有縣市", "基隆市",  "台北市", "新北市", "宜蘭縣", "桃園市", "新竹市", "新竹縣", "苗栗縣", "台中市", "彰化縣", "南投縣", "雲林縣", "嘉義市" ,"嘉義縣", "台南市", "高雄市", "屏東縣", "花蓮縣", "台東縣", "澎湖縣", "金門縣", "連江縣"];
+            cityData["option"] = [];
+            $("#cityValue").empty();
+            $("#city-popup-option-popup").remove();
+            var firstStringNum = 110;
+
+            //生成所有縣市列表
+            for (var i in cityList) {
+                var cityVal = i.toString();
+                cityData["option"][i] = {};
+                cityData["option"][i]["value"] = cityVal;
+                cityData["option"][i]["text"] = cityList[i];
+            }
+
+            $("#cityValue").empty();
+            tplJS.DropdownList("viewQStoreSearchList", "cityValue", "prepend", "typeB", cityData);
+        }
+
+        function getAllCategoryList() {
+            var  categoryList = [ "所有類別", "食",  "衣", "住", "行", "育", "樂", "其他"];
+            categoryData["option"] = [];
+            $("#categoryValue").empty();
+            $("#category-popup-option-popup").remove();
+
+            //生成所有種類列表
+            for (var i in categoryList) {
+                var categoryVal = i.toString();            
+                categoryData["option"][i] = {};
+                categoryData["option"][i]["value"] = categoryVal;
+                categoryData["option"][i]["text"] = categoryList[i];
+            }
+
+            $("#categoryValue").empty();
+            tplJS.DropdownList("viewQStoreSearchList", "categoryValue", "prepend", "typeB", categoryData);
         }
 
         /********************************** page event ***********************************/
-        $("#viewQStoreSearchList").on("pagebeforeshow", function (event, ui) {
-
-        });
 
         $("#viewQStoreSearchList").one("pageshow", function (event, ui) {
+            getAllCityList();
+            getAllCategoryList();
+        });
+
+        $("#viewQStoreSearchList").on("pagebeforeshow", function (event, ui) {
 
         });
 
