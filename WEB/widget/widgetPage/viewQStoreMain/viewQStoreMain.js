7a9f4de9814d409953cd1432b2b9f805ca8c66c6
jenniferywang <jennifer.y.wang@benq.com>
2018-12-12 19:28:00 +0800

    [QStore] Test google map

---
+++ WEB/widget/widgetPage/viewQStoreMain/viewQStoreMain.js
@@ -0,0 +1,222 @@
+
+$("#viewQStoreMain").pagecontainer({
+    create: function (event, ui) {
+
+        var food;
+        window.myLocate;
+        window.myLatLng;
+        window.allMarker = [];
+
+        /********************************** function *************************************/
+       
+        function geocodeAddress(geocoder, resultsMap, address, name) {
+            //var address = document.getElementById('address').value;
+            //var address = "台北市內湖區基湖路16號";
+
+            (function(geocoder, resultsMap, address, name) {
+                geocoder.geocode({'address': address}, function(results, status) {
+
+                    if (status === 'OK') {
+                        resultsMap.setCenter(results[0].geometry.location);
+
+                        /*
+                        var iconImage = {
+                            url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+                            size: new google.maps.Size(40, 62),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(0, 62)
+                        }
+
+                        var shape = {
+                            coords: [1, 1, 1, 20, 18, 20, 18, 1],
+                            type: 'poly'
+                        };
+                        */
+
+                        var iconImage = {
+                            url: "img/storepin.png",
+                            size: new google.maps.Size(20, 32),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(0, 32)
+                        }
+
+                        var marker = new google.maps.Marker({
+                        //window.marker = new google.maps.Marker({
+                            map: resultsMap,
+                            position: results[0].geometry.location,
+                            title: name,
+                            opacity: 1.0,
+                            attribution: {
+                                source: results[0].geometry.location.toString()
+                            },
+                            icon: iconImage
+                            //shape: shape
+                            //icon: "img/storepin.png"
+                        });
+
+
+                        window.allMarker.push(marker);
+
+                        //marker.setOpacity(1.0);
+                        window.allMarker.forEach(function(marker) {
+                            marker.setOpacity(1.0);
+                        });
+
+
+                        //Info Window
+                        var contentString = '<div id="content"> 這個地址是:' + address +'</div>';
+
+                        var infowindow = new google.maps.InfoWindow({
+                            content: contentString
+                        });
+
+                        marker.addListener('click', function() {
+                            //infowindow.open(resultsMap, marker);
+
+                            //marker.setIcon("img/markerB.png");
+                            //markerIcon(marker);
+
+                            markerInCenter(marker);
+
+                            //markerDetail(marker);
+                        });
+
+                        /*marker.addListener('icon_changed', function() {
+                            setTimeout(function(){
+                                markerText(marker);
+                            }, 500);
+                        });*/
+
+                        /*
+                        setTimeout(function(){
+                            window.marker.setMap(null);
+                        }, 10000);
+                        */
+
+                    } else {
+                        //alert('Geocode was not successful for the following reason: ' + status);
+                    }
+
+                });
+            }(geocoder, resultsMap, address, name));
+
+        }
+
+        function markerInCenter(nowMarker) {
+            window.map.setCenter(nowMarker.getPosition());
+            marker.setOpacity(1.0);
+        }
+
+        /********************************** page event ***********************************/
+
+        $("#viewQStoreMain").one("pageshow", function (event, ui) {
+            food = [{
+                name: "丹提咖啡",
+                distance: 0.3,
+                position: "(25.0810692, 121.5636862)",
+                address: "台北市內湖區堤頂大道二段407巷24號",
+                phone: "02 2658 4755"
+            }, {
+                name: "西雅圖極品咖啡",
+                distance: 0.017,
+                position: "(25.0810089, 121.56473299999993)",
+                address: "台北市內湖區基湖路18號",
+                phone: "02 8751 2128"
+            }, {
+                name: "星巴克",
+                distance: 0.074,
+                position: "(25.0803921, 121.56507120000003)",
+                address: "台北市內湖區基湖路25號",
+                phone: "02 2799 9334"
+            }, {
+                name: "珮斯坦咖啡館",
+                distance: 0.092,
+                position: "(25.0813135, 121.56576429999996)",
+                address: "台北市內湖區基湖路3巷1號1樓",
+                phone: "02 8797 8027"
+            }];
+
+        });
+
+        $("#viewQStoreMain").on("pageshow", function (event, ui) {
+                   console.log("=========== ready");
+
+                if (navigator.geolocation) {
+                    console.log("---------1");
+
+                    window.locationSuccess = function(position) {
+                        console.log("--------success");
+
+                        var pos = {
+                          lat: position.coords.latitude,
+                          lng: position.coords.longitude
+                        };
+
+                        console.log(pos);
+
+                        //Custom Marker icon
+                        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+                        var icons = {
+                            parking: {
+                                icon: iconBase + 'parking_lot_maps.png'
+                            },
+                            library: {
+                                icon: iconBase + 'library_maps.png'
+                            },
+                            info: {
+                                icon: iconBase + 'info-i_maps.png'
+                            }
+                        };
+
+                        //Google Map Marker
+                        window.myLatLng = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+
+                        if (typeof myLocate !== "undefined") {
+                            myLocate.setMap(null);
+                        }
+
+                        window.myLocate = new google.maps.Marker({
+                            position: myLatLng,
+                            map: window.map,
+                            icon: "img/icon_locationpin.png"
+                            //label: ""
+                        });
+
+                        setTimeout(function(){
+                            //set center
+                            window.map.setCenter(myLatLng);
+                        }, 2000);
+
+                    };
+
+                    window.locationError = function(error) {
+                        //console.log("------error");
+                        console.log(error);
+                        alert("------error");
+                    };
+
+                    //set food in marker
+                    for (var i=0; i<food.length; i++) {
+                        geocodeAddress(window.geocoder, window.map, food[i].address, food[i].name);
+                    }
+
+
+                    navigator.geolocation.getCurrentPosition(locationSuccess, locationError, {       
+                        enableHighAccuracy: true
+                    });
+                } else {
+                    console.log("---------2");
+                }
+        });
+
+        $("#viewQStoreMain").on("pagehide", function (event, ui) {
+
+        });
+
+        /********************************** dom event *************************************/
+
+    }
+});
