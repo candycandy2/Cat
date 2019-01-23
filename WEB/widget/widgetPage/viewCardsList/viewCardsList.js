12460aa18054b1d3c5253afedade423b63fa0635
jenniferywang <jennifer.y.wang@benq.com>
2019-01-23 18:03:00 +0800

    [QPlay] viewCardList Click Func + safety card slider

--- WEB/widget/widgetPage/viewCardsList/viewCardsList.js
+++ WEB/widget/widgetPage/viewCardsList/viewCardsList.js
@@ -13,5 +13,33 @@ $("#viewCardsList").pagecontainer({
 
         /********************************** dom event *************************************/
 
+        $(document).on('click', '#viewCardsList .qstore-card', function() {
+            let postData = {
+                'cardsType': "qstore"
+            }
+            checkWidgetPage('viewCardsMain', pageVisitedList, postData);
+        });
+
+        $(document).on('click', '#viewCardsList .medical-card', function() {
+            let postData = {
+                'cardsType': "medical"
+            }
+            checkWidgetPage('viewCardsMain', pageVisitedList, postData);
+        });
+
+        $(document).on('click', '#viewCardsList .quality-card', function() {
+            let postData = {
+                'cardsType': "quality"
+            }
+            checkWidgetPage('viewCardsMain', pageVisitedList, postData);
+        });
+
+        $(document).on('click', '#viewCardsList .safety-card', function() {
+            let postData = {
+                'cardsType': "safety"
+            }
+            checkWidgetPage('viewCardsMain', pageVisitedList, postData);
+        });
+
     }
 });
